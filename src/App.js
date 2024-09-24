aa3abc0235453d8fe7835214c446151495a0edc7
hovhannisyan2010 <hovhannisyanjora2010@gmail.com>
2024-09-24 23:27:06 +0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import * as math from "mathjs"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import "./App.css";
+import Button from "./Components/Button";
+import Input from "./Components/Input";
+
+const App = () => {
+  const [text, setText] = useState("")
+  const [result, setResult] = useState("")
+
+  const addToText = (val) => {
+    setText((text) => [...text, val + " "])
+  }
+
+  const resetInput = () => {
+    setText("")
+    setResult("")
+  }
+
+  const calculatorResult = () => {
+    if(text !== ""){
+      const input = text.join("")
+      setResult(math.evaluate(input))
+    }
+  }
+
+
+  const buttonColor = "#f2a33c"
+  return <div className="App">
+    <div className="calc-wrapper">
+      <Input text={text} result={result} />
+      <div className="row">
+        <Button symbol="7" handleClick={addToText} />
+        <Button symbol="8" handleClick={addToText} />
+        <Button symbol="9" handleClick={addToText} />
+        <Button symbol="/" color={buttonColor} handleClick={addToText}/>
+      </div>
+      <div className="row">
+        <Button symbol="4" handleClick={addToText}/>
+        <Button symbol="5" handleClick={addToText}/>
+        <Button symbol="6" handleClick={addToText}/>
+        <Button symbol="*" color={buttonColor} handleClick={addToText}/>
+      </div>
+      <div className="row">
+        <Button symbol="1" handleClick={addToText}/>
+        <Button symbol="2" handleClick={addToText}/>
+        <Button symbol="3" handleClick={addToText}/>
+        <Button symbol="+" color={buttonColor} handleClick={addToText}/>
+      </div>
+      <div className="row">
+        <Button symbol="0" handleClick={addToText}/>
+        <Button symbol="." handleClick={addToText}/>
+        <Button symbol="=" handleClick={calculatorResult}/>
+        <Button symbol="-" color={buttonColor} handleClick={addToText}/>
+      </div>
+      <Button symbol="Clear" color="red" handleClick={resetInput}/>
+
     </div>
-  );
-}
+  </div>
+};
 
 export default App;
